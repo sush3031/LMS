c0d27cba73e233db58f7ee5200ba8e4032b622fb
Sushmita Gupta <gupta.sushmita31@gmail.com>
2017-07-15 16:35:34 +0530

    LMS
    library management system

---
+++ Library Management System/src/javaconnect.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Sushmita Gupta
+ */
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+public class javaconnect {
+    
+    Connection conn;
+    
+    public static Connection ConnecrDb(){
+       try{
+         Class.forName("org.sqlite.JDBC");
+         Connection conn=DriverManager.getConnection("jdbc:sqlite:C:\\Users\\Sushmita Gupta\\Documents\\NetBeansProjects\\Library Management System\\LibraryNew.sqlite");
+         return conn;
+           
+       } catch(Exception e){
+           JOptionPane.showMessageDialog(null, e);
+           return null;
+       }
+        
+    }
+}
